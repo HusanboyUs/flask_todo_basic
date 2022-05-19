48f47585be8ec023e2e6492d3b3c217008f98797
Husanboy Usmonov <husanghost@gmail.com>
2022-05-19 03:24:44 +0200

    tempaltes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask, redirect,render_template,url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -11,13 +11,24 @@ class Todo(db.Model):
     content=db.Column(db.String(200), nullable=False)
     date_created=db.Column(db.DateTime, default=datetime.utcnow)
 
-
     def __repr__(self):
         return '<Task %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        pass
+        task_content=request.form['content']
+        new_task=Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect
+        except:
+            return "There was an issue while uploading your task"       
+    else:
+        tasks=Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
  
 
 if __name__ == "__main__":
